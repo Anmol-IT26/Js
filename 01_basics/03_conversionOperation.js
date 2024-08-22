e9dee1010a4a127142c0cb577f55c51510930ac9
Anmol-IT26 <tiwarianmol8397@gmail.com>
2024-08-22 17:12:34 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,3 +19,24 @@ console.log(isConvertInBool)
 "" => false
 "kaku" => true
 */
+//*************************operations *************** */
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)//power 
+//console.log(2%2)
+
+let str1 = "hello"
+let str2 = "kaku"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1"+ 2 + 2 )
+console.log(1 + 2 +" 2")
