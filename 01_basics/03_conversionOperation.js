b8405d9d57a2e1de65e54a26b57337d06b5691d2
Anmol-IT26 <tiwarianmol8397@gmail.com>
2024-08-22 16:44:07 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "anmol"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+/*
+conversion of values
+"33" = 33
+"33abc" = NaN
+true = 1
+false = 0
+*/
+let isBool = "kaku"
+let isConvertInBool = Boolean(isBool)
+console.log(isConvertInBool)
+/*
+"" => false
+"kaku" => true
+*/
