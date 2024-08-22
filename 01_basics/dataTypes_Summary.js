0a044816c61b7d95eba434f3930354ab69ccb5ab
Anmol-IT26 <tiwarianmol8397@gmail.com>
2024-08-22 17:45:05 +0000

    Data Tpes Summary

---
+++ 01_basics/dataTypes_Summary.js
@@ -0,0 +1,43 @@
+//Primitive
+
+// 7 types :String , Number,Boolean, null, undefinded, Symbol,BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const IsLoggedIn = false
+const temp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//Refrence type
+
+//Array,Objects,Fuctions
+const heroes = ["shaktiman","naagraj","doga"]
+
+ let myObj = {
+    name:"kaku",
+    age:22,
+}
+
+const myFunction = function(){
+     console.log("hello world")
+}
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       */
